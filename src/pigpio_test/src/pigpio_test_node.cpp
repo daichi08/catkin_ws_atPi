fd9ae3ebcd1c147a75965bd3cb8797d6312a2976
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2018-07-12 14:20:10 +0900

    rplidar使えるように

--- src/pigpio_test/src/pigpio_test_node.cpp
+++ src/pigpio_test/src/pigpio_test_node.cpp
@@ -1,24 +1,51 @@
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
+#include "pigpiod_if2.h"
 
 #define RAD2DEG(x) ((x)*180./M_PI)
 
+float distance = 0;
+float angle;
+
+int pi;
+extern int pi;
+
+static int pwmpin[2] = {18, 19};
+static int dirpin[2] = {21, 20};
+
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
-    int count = scan->scan_time / scan->time_increment;
-    ROS_INFO("I heard a laser scan %s[%d]:", scan->header.frame_id.c_str(), count);
-    ROS_INFO("angle_range, %f, %f", RAD2DEG(scan->angle_min), RAD2DEG(scan->angle_max));
-  
-    for(int i = 0; i < count; i++) {
-        float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i);
-        ROS_INFO(": [%f, %f]", degree, scan->ranges[i]);
+  int count = scan->scan_time / scan->time_increment;
+
+  for(int i = 0; i < count; i++) {
+    float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i);
+      if(90.0 <= degree || degree <= -90.0){
+        if(distance == 0){
+          distance = scan->ranges[i];
+          angle =  degree;
+        }else{
+          if(distance > scan->ranges[i]){
+            distance = scan->ranges[i];
+            angle =  degree;
+          }
+        }
     }
+  }
+  ROS_INFO(": [%f, %f]", distance, angle);
+  distance = 0;
+  angle = 0;
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "rplidar_node_client");
     ros::NodeHandle n;
+
+    pi = pigpio_start("localhost","8888");
+    set_mode(pi, pwmpin[0], PI_OUTPUT);
+    set_mode(pi, dirpin[0], PI_OUTPUT);
+    set_mode(pi, pwmpin[1], PI_OUTPUT);
+    set_mode(pi, dirpin[1], PI_OUTPUT);
 
     ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, scanCallback);
 
