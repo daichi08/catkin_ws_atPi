5d049cc234422ff6f95fed8db2a1e521c72686c5
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2018-07-12 14:00:18 +0900

    clientnodeと同一に

--- src/pigpio_test/src/pigpio_test_node.cpp
+++ src/pigpio_test/src/pigpio_test_node.cpp
@@ -6,14 +6,13 @@
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
     int count = scan->scan_time / scan->time_increment;
+    ROS_INFO("I heard a laser scan %s[%d]:", scan->header.frame_id.c_str(), count);
+    ROS_INFO("angle_range, %f, %f", RAD2DEG(scan->angle_min), RAD2DEG(scan->angle_max));
   
     for(int i = 0; i < count; i++) {
         float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i);
-        if(90.0 <= degree || degree <= -90.0){
-          ROS_INFO(": [%f, %f]", degree, scan->ranges[i]);
-        }
+        ROS_INFO(": [%f, %f]", degree, scan->ranges[i]);
     }
-    ROS_INFO(": [%f, %f]", distance, angle);
 }
 
 int main(int argc, char **argv)
